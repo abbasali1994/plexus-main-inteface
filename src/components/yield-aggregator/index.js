cbc1a133f97e82ea0961aa6f29105815c2c8922a
terminator0x <mulish.tc@gmail.com>
2021-06-15 22:10:57 +0300

    added the yield page

---
+++ src/components/yield-aggregator/index.js
@@ -0,0 +1,7 @@
+import './index.scss';
+
+const YieldAggregator = () => { 
+    return <div></div>
+};
+
+export default YieldAggregator;
---
+++ src/pages/yield/index.js
@@ -0,0 +1,22 @@
+import { Row, Col } from 'react-bootstrap';
+
+import Header from '../../components/header';
+import YieldAggregator from '../../components/yield-aggregator';
+import Sidebar from '../../components/sidebar';
+
+const YieldAggregatorPage = () => {
+
+    return (
+        <Row>
+            <Col lg={9}>
+                <Header/>
+                <YieldAggregator/>
+            </Col>
+            <Col lg={3}>
+                <Sidebar/>
+            </Col>
+        </Row>
+    );
+}
+
+export default YieldAggregatorPage;
