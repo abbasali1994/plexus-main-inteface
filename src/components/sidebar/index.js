f1223c4d2359b022fa4dd938e53697540b51a0db
Magic Dev <magicrjh0619@gmail.com>
2021-07-02 04:16:16 +1000

    Sidebar styling (#49)
    * sidebar style

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -30,7 +30,7 @@ const Sidebar = () => {
                     <Col lg="2">
                         <Image src={LogoIcon}></Image>
                     </Col>
-                    <Col>
+                    <Col className="p-0">
                         <span className="address-text">0X1E....D396</span>
                         <FontAwesomeIcon className="caret-down" icon={faCaretDown} />
                     </Col>
@@ -102,7 +102,7 @@ const MainSidebar = () => {
             <Container className="description-row">
                 <Row>
                     <Col lg="2" className="description-part"></Col>
-                    <Col className="ml-4 mr-4">
+                    <Col className="ml-4 mr-2">
                         <div className="subtitle-text">ASSETS OVERVIEW</div>
                         <div className="description-text">Click any category to see the breakdown of these assets in your portfolio, and perform related actions.</div>
                     </Col>
@@ -111,7 +111,7 @@ const MainSidebar = () => {
             <Container className="description-yield">
                 <Row>
                     <Col lg="2" className="description-part"></Col>
-                    <Col className="ml-4 mr-4">
+                    <Col className="ml-4 mr-2">
                         <div className="subtitle-text">YIELD FARMING</div>
                         <div className="description-text">Select any farm you are participating in to manage your position and claim any available rewards you’ve earned.</div>
                     </Col>
@@ -131,7 +131,7 @@ const LiquiditySidebar = () => {
             <Container className="description-row">
                 <Row>
                     <Col lg="2" className="description-part"></Col>
-                    <Col className="ml-4 mr-4">
+                    <Col className="ml-4 mr-2">
                         <div className="subtitle-text">PROTOCOLS</div>
                         <div className="description-text">View your positions across all protocols at a glance, or select a specific one to see all your positions for that platform.</div>
                     </Col>
@@ -140,7 +140,7 @@ const LiquiditySidebar = () => {
             <Container className="description-yield">
                 <Row>
                     <Col lg="2" className="description-part"></Col>
-                    <Col className="ml-4 mr-4">
+                    <Col className="ml-4 mr-2">
                         <div className="subtitle-text">YOUR POSITIONS</div>
                         <div className="description-text">Select a position to view stats, information, and perform available actions like staking, unwrapping and remixing.</div>
                     </Col>
@@ -166,39 +166,39 @@ const YieldSidebar = () => {
                         <div className="subtitle-text-yield">PLX/ETH SUSHISWAP LP</div>
                     </Col>
                 </Row>
-                <Row>
-                    <Col lg={6}>
+                <Row className="mt-3">
+                    <Col lg={6} className="pr-0">
                         <div className="yield-info-box">
-                            <div className = "yield-info-title-text">Total Deposited</div>   
-                            <div className = "yield-info-value-text">18,324,543.21</div>
+                            <div className="yield-info-title-text">Total Deposited</div>   
+                            <div className="yield-info-value-text">$18,324,543.21</div>
                         </div>
                     </Col>
                     <Col lg={6}>
                         <div className="yield-info-box">
-                            <div className = "yield-info-title-text">ROI</div>   
-                            <div className = "yield-info-value-text">45.6% (1Y)</div>
+                            <div className="yield-info-title-text">ROI</div>   
+                            <div className="yield-info-value-text">45.6% (1Y)</div>
                         </div>
                     </Col>
                 </Row>
                 <Row className="mt-4">
-                    <Col lg={6}><div className="yield-personal-info-title-text">Your Deposites:</div></Col>
+                    <Col lg={6} className="pr-0"><div className="yield-personal-info-title-text">Your Deposites:</div></Col>
                     <Col lg={6} className="pr-2"><div className="yield-personal-info-value-text">0.00 SLP</div></Col>
                 </Row>
                 <Row>
-                    <Col lg={6}><div className="yield-personal-info-title-text">Rewards Earned:</div></Col>
-                    <Col lg={6} className="pr-2"><div className="yield-personal-info-value-text p-0"><img src={Rat} alt="" className="mb-1 mr-1"/><span className="mr-3">0.00</span><img src={Sushi} alt="" className="mb-1 mr-1"/><span>0.00</span></div></Col>
+                    <Col lg={6} className="pr-0"><div className="yield-personal-info-title-text">Rewards Earned:</div></Col>
+                    <Col lg={6} className="pr-2 pl-0"><div className="yield-personal-info-value-text p-0"><img src={Rat} alt="" className="mb-1 mr-1"/><span className="mr-3">0.00</span><img src={Sushi} alt="" className="mb-1 mr-1"/><span>0.00</span></div></Col>
                 </Row>
-                <Row>
-                    <Col className = "yield-info-box-seperator"></Col>
+                <Row className="mt-3">
+                    <Col className="yield-info-box-seperator"></Col>
                 </Row>
             </Container>
             <Container className="px-3 yield-action">
                 <Row className="mt-4">
                     <Col >
                         <div className="yield-action-button-box">
-                            <div className={buttonState === 'deposit' ? "yield-action-button-text-box active" : "yield-action-button-text-box"} onClick={() => setButtonState('deposit')}>Deposit</div>
-                            <div className={buttonState === 'withdraw' ? "yield-action-button-text-box active" : "yield-action-button-text-box"} onClick={() => setButtonState('withdraw')}>Withdraw</div>
-                            <div className={buttonState === 'claim' ? "yield-action-button-text-box active" : "yield-action-button-text-box"} onClick={() => setButtonState('claim')}>Claim Rewards</div>
+                            <div className={buttonState === 'deposit' ? "w-30 yield-action-button-text-box active" : "w-30 yield-action-button-text-box"} onClick={() => setButtonState('deposit')}>Deposit</div>
+                            <div className={buttonState === 'withdraw' ? "w-30 yield-action-button-text-box active" : "w-30 yield-action-button-text-box"} onClick={() => setButtonState('withdraw')}>Withdraw</div>
+                            <div className={buttonState === 'claim' ? "w-40 yield-action-button-text-box active" : "w-40 yield-action-button-text-box"} onClick={() => setButtonState('claim')}>Claim Rewards</div>
                         </div>
                     </Col>
                 </Row>
@@ -234,7 +234,7 @@ const WalletSidebar = () => {
             <Container className="description-yield">
                 <Row>
                     <Col lg="2" className="description-part"></Col>
-                    <Col className="ml-4 mr-4">
+                    <Col className="ml-4 mr-2">
                         <div className="subtitle-text">YOUR TOKENS</div>
                         <div className="description-text">Select a token to view stats, information, and available actions to perform.</div>
                     </Col>
@@ -254,7 +254,7 @@ const RewardsSidebar = () => {
             <Container className="description-row mt-6e">
                 <Row>
                     <Col lg="2" className="description-part"></Col>
-                    <Col className="ml-4 mr-4">
+                    <Col className="ml-4 mr-2">
                         <div className="subtitle-text">YOUR REWARDS</div>
                         <div className="description-text">Select an active yield farm you’re participating in to claim your earned PLX and/or protocol token rewards.</div>
                     </Col>
@@ -274,7 +274,7 @@ const StakingSidebar = () => {
             <Container className="description-row mt-6e">
                 <Row>
                     <Col lg="2" className="description-part"></Col>
-                    <Col className="ml-4 mr-4">
+                    <Col className="ml-4 mr-2">
                         <div className="subtitle-text">YOUR STAKING</div>
                         <div className="description-text">Select an active yield farm you’re participating in to claim your earned PLX and/or protocol token rewards.</div>
                     </Col>
@@ -288,7 +288,7 @@ const YieldMainSidebar = () => {
     return (
         <Container className="info-container">
             <Container className="description-row mt-1">
-                <Col className="ml-4 mr-4 p-4">
+                <Col className="ml-2 mr-4 p-4">
                     <div className="subtitle-text-white">YIELD AGGREGATOR</div>
                     <div className="description-text">View and enter yield farms from popular protocols with quick zap in functions and competitive ROIs.</div>
                 </Col>
@@ -296,7 +296,7 @@ const YieldMainSidebar = () => {
             <Container className="description-row">
                 <Row>
                     <Col lg="2" className="description-part"></Col>
-                    <Col className="ml-4 mr-4">
+                    <Col className="ml-4 mr-2 p-0">
                         <div className="subtitle-text">PROTOCOLS</div>
                         <div className="description-text">Select any protocol to view the available yield opportunities and perform any available actions.</div>
                     </Col>
@@ -305,7 +305,7 @@ const YieldMainSidebar = () => {
             <Container className="description-yield">
                 <Row>
                     <Col lg="2" className="description-part"></Col>
-                    <Col className="ml-4 mr-4">
+                    <Col className="ml-4 mr-2 p-0">
                         <div className="subtitle-text">YIELD OPPORTUNITIES</div>
                         <div className="description-text">See the various farming opportunities for your selected protocol, see what reward tokens are offered, and their ROI.</div>
                     </Col>
