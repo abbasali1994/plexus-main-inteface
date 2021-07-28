b372c4109ffa4dca0a626ed9df64fdbf118804b2
Magic Dev <magicrjh0619@gmail.com>
2021-07-28 13:55:22 +1000

    Created Pie component

---
+++ src/components/pie-chart/index.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import "./index.scss";
+import { PieChart } from 'react-minimal-pie-chart';
+
+import { currentTheme } from '../../redux/themeSlice';
+
+const Pie = ({ percentage, size }) => {
+  
+  useEffect(() => {
+    const root = document.documentElement;
+    root.style.setProperty("--pie-height", size + "px");
+  }, [size]);
+
+  const theme = useSelector(currentTheme);
+  console.log(theme);
+  
+  const data = [
+    { title: 'First', value: percentage, color: (theme === "light" ? '#4ea4d3' : '#236EDC') },
+    { title: 'Second', value: 100.0 - percentage, color: (theme === "light" ? '#C4C4C4' : '#4F4F4F') },
+  ];
+
+  return (
+    <div>
+      <PieChart data={data} className="pie-chart"/>
+    </div>
+  )
+};
+
+export default Pie;
