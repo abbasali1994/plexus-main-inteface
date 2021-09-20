568716501a4bb0a2cd29746a6ff643e684e383a2
abbasali1994 <meetabbasali1994@gmail.com>
2021-09-20 19:19:28 +0530

    Added Api calls to fetch token details in redux state on init

--- src/redux/pricesSlice.js
+++ src/redux/pricesSlice.js
@@ -1,24 +1,62 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const pricesSlice = createSlice({
+export const getTokenUSDPrices = createAsyncThunk(
+  "prices/getTokenUSDPrices",
+  async (tokens) => {
+    return fetch(
+      `https://api.coingecko.com/api/v3/simple/price?ids=${tokens}&vs_currencies=usd`
+    ).then((res) => res.json());
+  }
+);
+
+export const getGasPrices = createAsyncThunk(
+  "prices/getGasPrices",
+  async () => {
+    return fetch(
+      `https://www.gasnow.org/api/v3/gas/price?utm_source=:plexus`
+    ).then((res) => res.json());
+  }
+);
+
+const pricesSlice = createSlice({
   name: "prices",
   initialState: {
-    tokens: {},
-    lpPositions: {},
+    coinGeckoApiStatus: null,
+    gasNowStatus: null,
+    pricesUSD: {},
+    gasPrices: {},
   },
   reducers: {
-    setTokens: (state, action) => {
-      state.tokens = action.payload;
+    updateTokenPrice: (state, { payload }) => {
+      console.log(payload);
+      state.pricesUSD = { ...state.pricesUSD, ...payload };
     },
-    setLpPositions: (state, action) => {
-      state.lpPositions = action.payload;
+  },
+  extraReducers: {
+    [getTokenUSDPrices.pending]: (state, _action) => {
+      state.coinGeckoApiStatus = "loading";
+    },
+    [getTokenUSDPrices.fulfilled]: (state, { payload }) => {
+      state.pricesUSD = payload;
+      state.coinGeckoApiStatus = "success";
+    },
+    [getTokenUSDPrices.rejected]: (state, _action) => {
+      state.coinGeckoApiStatus = "failed";
+    },
+
+    [getGasPrices.pending]: (state, _action) => {
+      state.gasNowStatus = "loading";
+    },
+    [getGasPrices.fulfilled]: (state, { payload }) => {
+      state.gasPrices = payload.data;
+      state.gasNowStatus = "success";
+    },
+    [getGasPrices.rejected]: (state, _action) => {
+      state.gasNowStatus = "failed";
     },
   },
 });
 
-// Action creators are generated for each case reducer function
-export const { setTokens, setLpPositions } = pricesSlice.actions;
-
-export const tokenPrices = (state) => state.wallet.tokens;
+export const { updateTokenPrice } = pricesSlice.actions;
 
 export default pricesSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -2,11 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import sidebarReducer from "./redux/sidebarSlice";
 import themeReducer from "./redux/themeSlice";
 import walletReducer from "./redux/walletSlice";
+import pricesReducer from "./redux/pricesSlice";
+import tokensReducer from "./redux/tokensSlice";
 
 export default configureStore({
   reducer: {
     sidebar: sidebarReducer,
     theme: themeReducer,
     wallet: walletReducer,
+    prices: pricesReducer,
+    tokens: tokensReducer,
   },
 });
